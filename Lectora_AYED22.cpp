c48c7b28dd15fff0e0054e2905d93c26893f2124
JS <javier_sinuka@mi.unc.edu.ar>
2022-04-25 15:17:05 -0300

    Creacion archivos TP1

---
+++ Lectora_AYED22.cpp
@@ -0,0 +1,5 @@
+//
+// Created by javie on 25/04/2022.
+//
+
+#include "Lectora_AYED22.h"
---
+++ Lectora_AYED22.h
@@ -0,0 +1,14 @@
+//
+// Created by javie on 25/04/2022.
+//
+
+#ifndef TP_1_ALGORITMOS_LECTORA_AYED22_H
+#define TP_1_ALGORITMOS_LECTORA_AYED22_H
+
+
+class Lectora_AYED22 {
+
+};
+
+
+#endif //TP_1_ALGORITMOS_LECTORA_AYED22_H
